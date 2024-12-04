f94fb5af8c3537dce10e85746b27d1dd5e290649
Leslie Barron <lesliebarron1@gmail.com>
2024-12-03 22:52:35 -0500

    Update day1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,28 +1,20 @@
 module Day01 (solve) where
 
-import Data.List.Split (splitOn)
+import Control.Arrow ((>>>))
 import Data.List (sort)
 
-part1 :: [Int] -> [Int] -> Int
-part1 (x:xs) (y:ys) = abs (x - y) + part1 xs ys
-part1 [] [] = 0
+type Input = ([Int], [Int])
 
-part2 :: [Int] -> [Int] -> Int
-part2 (x:xs) ys = 
-   let count = length $ filter (x==) ys in
-   x * count + part2 xs ys
-part2 [] _ = 0
+part1 :: Input -> Int
+part1 (xs, ys) = sum $ map (\(x, y) -> abs (x - y)) $ zip (sort xs) (sort ys)
 
-parse :: ([Int], [Int]) -> String -> ([Int], [Int])
-parse (xs, ys) line =
-    case (words line) of
-        [x, y] -> ((read x :: Int) : xs, (read y :: Int) : ys)
-        _ -> error $ "unexpected line: " ++ line 
+part2 :: Input -> Int
+part2 (xs, ys) = sum $ map (\x -> x * (length $ filter (x==) ys)) xs
+
+parse :: String -> Input
+parse = lines >>> map (line . words) >>> unzip
+    where line [x, y] = (read x, read y)
+          line invalid = error $ show invalid
 
 solve :: String -> String
-solve input =
-    let lines = filter (not . null) $ splitOn "\n" input in
-    let (xs, ys)  = foldl parse ([], []) lines in
-    let p1 = show $ part1 (sort xs) (sort ys) in
-    let p2 = show $ part2 xs ys in
-    p1 ++ " " ++ p2
+solve = parse >>> (\x -> (part1 x, part2 x)) >>> show
